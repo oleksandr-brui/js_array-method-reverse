dd1c0cd5e691f2d391c514c091a659671c338694
Oleksandr Brui <oleksandr.bruigit@gmail.com>
2021-03-23 00:00:28 +0200

    Implementation of the method .reverse() in native Java Script

--- js-arrayMethodReverse.js
+++ js-arrayMethodReverse.js
@@ -1,1 +1,18 @@
 'use strict';
+
+numbers.reverse = function() {
+  // write code here
+  this.length *= 2;
+
+  for (let i = 0; i < this.length; i++) {
+    this[this.length - 1 - i] = this[i];
+  }
+
+  for (let j = 0; j < this.length / 2; j++) {
+    this[j] = this[j + this.length / 2];
+  }
+
+  this.length = this.length / 2;
+
+  return this;
+};
